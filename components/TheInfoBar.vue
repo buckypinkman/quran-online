cb041beae3a1c57ddc46658fa1ccc5d0c9ad81bc
buckypinkman <rangga7765@gmail.com>
2021-01-16 14:47:01 +0700

    change infobar close button font weight

--- components/TheInfoBar.vue
+++ components/TheInfoBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="info max-w-md flex justify-between mx-auto bg-blue-100 text-blue-800 font-semibold rounded-md shadow overflow-hidden md:max-w-2xl mt-6 p-6" v-if="showInfo">
       <p >Klik pada teks arab untuk putar audio</p>
-      <p @click="showInfo = false" class="cursor-pointer">x</p>
+      <p @click="showInfo = false" class="cursor-pointer font-bold">x</p>
     </div>
 </template>
 
