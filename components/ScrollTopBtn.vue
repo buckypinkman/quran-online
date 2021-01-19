85a1f1533e0e25ca535144e73421269e366a8665
buckypinkman <rangga7765@gmail.com>
2021-01-19 15:49:32 +0700

    add svg

--- components/ScrollTopBtn.vue
+++ components/ScrollTopBtn.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="bg-white shadow-xl mr-4 pt-3 rounded-md fixed bottom-0 right-0 text-white mb-4 cursor-pointer bg-indigo-400 hover:bg-indigo-500 transition duration-200 text-center"
+    class="bg-white shadow-xl mr-4 pt-4 rounded-md fixed bottom-0 right-0 text-white mb-4 cursor-pointer bg-indigo-400 hover:bg-indigo-500 transition duration-200 text-center"
     @click.prevent="scrollTop"
   >
-    <a class="font-bold text-2xl"> ^ </a>
+    <img src="../assets/icons/up-arrow.svg" alt="arrow up" class="m-auto">
   </div>
 </template>
 
@@ -19,7 +19,11 @@ export default {
 
 <style scoped>
 div {
-  width: 3.4rem;
-  height: 3.7rem
+  width: 3.7rem;
+  height: 3.4rem
+}
+
+img {
+  width: 20px;
 }
 </style>
