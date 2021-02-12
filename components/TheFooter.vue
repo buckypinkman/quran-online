efc8e79620016ab518ecb3fc627fb195a58d2fdc
buckypinkman <rangga7765@gmail.com>
2021-02-12 15:15:24 +0700

    add footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="footer text-center pt-6 dark:text-gray-200">
+        <p>Made with ❤️ by <a href="https://web.facebook.com/rangga.dimas80/" class="text-indigo-300" target="_blank">Rangga Dimas</a></p>
+    </div>
+</template>
