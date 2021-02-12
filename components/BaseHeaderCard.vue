040240b064a9d804b4367674ef9ef604188d2e24
buckypinkman <rangga7765@gmail.com>
2021-02-12 15:19:08 +0700

    change directory

--- components/BaseHeaderCard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div
-      class="max-w-md mx-auto bg-white rounded-md shadow-lg overflow-hidden md:max-w-2xl mt-6 p-5 dark:bg-gray-800 dark:text-gray-300"
-    >
-        <slot></slot>
-    </div>
-</template>
