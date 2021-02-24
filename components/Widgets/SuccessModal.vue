8c1139a2ffc212d80467302bf7fba0d3a344cde1
buckypinkman <rangga7765@gmail.com>
2021-02-24 18:42:14 +0700

    delete data, add slot

--- components/Widgets/SuccessModal.vue
+++ components/Widgets/SuccessModal.vue
@@ -2,18 +2,11 @@
   <transition name="fade">
     <div
       class="sucess-modal px-4 py-3 text-sm md:text-base z-10 font-semibold fixed bottom-0 md:mb-24 mb-20 bg-green-300 dark:bg-green-400 rounded-md shadow-lg text-white"
-      v-if="showModal"
     >
-      <p>Sukses menambahkan sebagai ayat terakhir dibaca</p>
+      <slot></slot>
     </div>
   </transition>
 </template>
-
-<script>
-export default {
-  props: ["showModal"],
-};
-</script>
 
 <style scoped>
 .fade-enter-active,
