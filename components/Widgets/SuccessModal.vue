325e1660e251154e02b7c3c682cf21262bb51ea6
buckypinkman <rangga7765@gmail.com>
2021-03-25 14:50:51 +0700

    change animation

--- components/Widgets/SuccessModal.vue
+++ components/Widgets/SuccessModal.vue
@@ -1,11 +1,9 @@
 <template>
-  <transition name="fade">
     <div
-      class="sucess-modal px-4 py-3 text-sm md:text-base z-50 font-semibold fixed bottom-0 md:mb-24 mb-20 bg-green-300 dark:bg-green-400 rounded-md shadow-lg text-white"
+      class="sucess-modal px-4 py-3 text-sm md:text-base z-50 font-semibold fixed bottom-0 md:mb-24 mb-20 bg-green-400 dark:bg-green-500 rounded-md shadow-lg text-white"
     >
       <slot></slot>
     </div>
-  </transition>
 </template>
 
 <style scoped>
