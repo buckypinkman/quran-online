175ba05442ac906f97cc04c9251c48934060b9be
buckypinkman <rangga7765@gmail.com>
2021-03-25 14:47:19 +0700

    fix size on mobile

--- components/Widgets/ScrollTopBtn.vue
+++ components/Widgets/ScrollTopBtn.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="bg-white shadow-xl mr-4 pt-4 rounded-md fixed bottom-0 right-0 text-white md:mb-3 mb-20 cursor-pointer bg-indigo-400 hover:bg-indigo-500 transition duration-200 text-center dark:bg-indigo-500"
+    class="shadow-xl mr-4 rounded-md fixed bottom-0 right-0 text-white mb-20 cursor-pointer opacity-75 bg-indigo-400 hover:bg-indigo-500 transition duration-200 text-center dark:bg-indigo-500"
     @click.prevent="scrollTop"
   >
-    <img src="../../assets/icons/up-arrow.svg" alt="arrow up" class="m-auto">
+    <img src="../../assets/icons/up-arrow.svg" alt="arrow up" class="m-auto" />
   </div>
 </template>
 
@@ -19,8 +19,17 @@ export default {
 
 <style scoped>
 div {
-  width: 3.7rem;
-  height: 3.4rem
+  width: 3.3rem;
+  height: 3rem;
+  padding-top: 0.8rem;
+}
+
+@media only screen and (min-width: 600px) {
+  div {
+    width: 3.7rem;
+    height: 3.4rem;
+    padding-top: 1rem;
+  }
 }
 
 img {
