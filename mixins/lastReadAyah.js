484d750c982885ade1068a75102b63743bdec333
buckypinkman <rangga7765@gmail.com>
2021-04-21 17:09:36 +0700

    refactor

--- mixins/lastReadAyah.js
+++ mixins/lastReadAyah.js
@@ -1,26 +1,21 @@
+import {
+  addToLocal
+} from './../utils/index'
+
 export default {
   data() {
     return {
       last_read_ayah: [],
-      showModal: false,
     }
   },
   methods: {
-    addAyah(ayah, surahNumber) {
-      this.last_read_ayah.push({
-        ayah: ayah,
-        surahNumber: surahNumber
-      });
-      this.saveAyah();
-      this.toggleModal();
-    },
-    toggleModal() {
-      this.showModal = true;
-      setTimeout(() => (this.showModal = false), 1800);
-    },
-    saveAyah() {
-      const parsed = JSON.stringify(this.last_read_ayah);
-      localStorage.setItem("ayah", parsed);
+    addAyah(ayah) {
+      if (this.last_read_ayah) {
+        this.last_read_ayah.splice(0, 1)
+      }
+      this.last_read_ayah.unshift(ayah);
+      addToLocal(this.last_read_ayah, "ayah")
+      this.$store.commit('toggleModal');
     },
   },
 }
