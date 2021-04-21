ea5780a2026c72f4636f17525ddb372311ee2e63
buckypinkman <rangga7765@gmail.com>
2021-04-21 23:07:46 +0700

    get text from store

--- components/Widgets/SuccessModal.vue
+++ components/Widgets/SuccessModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div
-      class="sucess-modal px-4 py-3 text-sm md:text-base z-50 font-semibold fixed bottom-0 md:mb-24 mb-20 bg-green-400 dark:bg-green-500 rounded-md shadow-lg text-white"
+      class="sucess-modal opacity-8 px-4 py-3 text-sm md:text-base z-50 font-semibold fixed bottom-0 md:mb-24 mb-20 bg-green-400 dark:bg-green-500 rounded-md shadow-lg text-white"
     >
-      <slot></slot>
+      <p>{{ $store.state.modal_title }}</p>
     </div>
 </template>
